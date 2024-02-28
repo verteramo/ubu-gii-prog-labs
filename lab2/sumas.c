f314e49b0b0e4108cb49302866b07bceaa18a70b
verteramo <verteramo@nr200p>
2024-02-28 16:23:01 +0100

    Correcciones

--- lab2/sumas.c
+++ lab2/sumas.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#define M_PI 3.14 
+#define M_PI 3.14
 int main(){ /*prg. ppal.*/
   int n1, n2, n3, n4, n5, n6,
-        result;
+      result;
     /* E/ */
   printf("Introduce 5 enteros entre espacios\n");
   scanf("%d%d%d%d%d", &n1, &n2, &n3, &n4, &n5);
-
     /*  /S*/
   printf("%11d\n", n1);
   printf("%11d\n", n2);
@@ -18,6 +17,5 @@ int main(){ /*prg. ppal.*/
   scanf("%d", &result);
   printf("Salida.\n");
   printf("%11d respuestas\n%11d solucion\n", result, n1+n2+n3+n4+n5);
-
   return 0;
 }
